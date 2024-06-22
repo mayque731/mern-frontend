a591944473a5b3d377ef4686812740b4b629014c
Mayque Oliveira <mayque.731@gmail.com>
2024-06-22 11:33:27 -0400

    maps api failed

---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Map.css'
+const Map = props => {
+    // const mapRef = useRef();
+    // const {center,zoom, mapId} = props;
+    // useEffect(() => {
+    //     const map = new window.google.maps.Map(mapRef.current,{
+    //         center: center,
+    //         zoom: zoom,
+    //         mapId: mapId
+    // }); 
+    // new window.google.maps.marker.AdvancedMarkerElement({position: center, map: map, mapId: "mapId"});
+    // },[center,zoom]);
+    // return <div ref={mapRef} className={`map ${props.className}`} style={props.style}>
+    
+    // </div>
+    
+};
+
+export default Map;
