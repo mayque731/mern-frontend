f16ef33217f5365d1dc50b43437ac2c3150450c5
Mayque Oliveira <mayque.731@gmail.com>
2024-06-24 16:14:15 -0400

    validators added

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,15 @@
 import React from "react";
 import './NewPlace.css';
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 
 const NewPlace = () => {
     return <form className="place-form">
-        <Input element ="input" type="text" label="Title" validators={[]} errorText="Input a valid title" />
+        <Input element ="input" 
+        type="text" 
+        label="Title" v
+        alidators={[VALIDATOR_REQUIRE()]} 
+        errorText="Input a valid title" />
         </form>;
 };
 
